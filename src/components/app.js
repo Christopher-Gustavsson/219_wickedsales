effbfe895c10188684a5a504d35597dfa476219d
Christopher-Gustavsson <chrisgustavsson1@gmail.com>
2019-04-08 17:02:53 -0700

    Completed 404 page for client

--- src/components/app.js
+++ src/components/app.js
@@ -3,17 +3,21 @@ import 'materialize-css/dist/js/materialize.min'
 import '../assets/css/app.scss';
 import React from 'react';
 import ProductRoutes from './products'; //will automatically look for an index file aka index.js inside products folder
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Home from './home';
 import Nav from './nav';
+import NotFound from './404';
 
 
 const App = () => (
     <div>
         <Nav />
         <div className="container">
-            <Route exact path="/" component={Home} />
-            <Route path="/products" component={ProductRoutes} />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/products" component={ProductRoutes} />
+                <Route  component={NotFound} />
+            </Switch>
         </div>
     </div>
 );
